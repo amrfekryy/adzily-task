bbfab2e75c473f9b00b8ff13f085b1658750ebb8
Amr-Fekry-Bosila <amrfekryy@gmail.com>
2021-07-15 04:07:16 +0200

    fix

--- src/App.js
+++ src/App.js
@@ -6,10 +6,10 @@ import { useApp } from './helpers'
 
 export default function App() {
   const appStore = useApp(sampleData)  
-  // const {
-  //   singers, albums, songs, totals, form,
-  //   setSinger, setAlbum, setSong
-  // } = appStore
+  const {
+    singers, albums, songs, totals, form,
+    setSinger, setAlbum, setSong
+  } = appStore
 
   // console.log('singers', singers)
   // console.log('albums', albums)
